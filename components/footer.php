cf029299ce6f35b97e8f83131f7d9755b6dc2fec
Maria Ilade <mariailadem@gmail.com>
2025-06-05 22:40:34 +0300

    Made the footer that will be used for mostly every page

---
+++ components/footer.php
@@ -0,0 +1,37 @@
+
+
+<footer class="footer">
+    <section class="information">
+
+        <section class="information-block"> 
+        <p class="title-text"> How Can We Help? </p>
+        <nav class="nav-list"> <!-- TO DO: ADD relevant links -->
+            <a href="#">Adopt a pet</a>
+            <a href="#">Rehome a pet</a>
+            <a href="#">Adopt FAQ's</a>
+            <a href="#">Rehome FAQ's</a>
+        </nav> <!-- TO DO: ADD relevant links -->
+        </section>
+
+        <section class="information-block"> 
+        <p class="title-text"> Contact Us </p>
+        <nav class="nav-list"> <!-- TO DO: ADD relevant links -->
+            <a href="#">Strada General Henri Mathias Berthelot 16, Iași</a>
+            <a href="#">+40 712 345 678</a>
+            <a href="#">johndoe@gmail.com</a>
+        </nav> <!-- TO DO: ADD relevant links -->
+        </section>
+
+        <section class="information-block"> 
+        <p class="title-text"> Keep In Touch With Us </p>
+            <div class="nav-list"> <!-- TO DO: ADD relevant links -->
+            <p>Join the FurryFriends magazine and be first to hear about news</p>
+            <a href="#">TO DO: PUT TEXTBOX</a>
+            </div> <!-- TO DO: ADD relevant links -->
+        </section>
+        
+    </section>
+    <section class="socials">
+        <p class="socials-text"> @2025 FurryFriends </p>
+    </section>
+</footer>
