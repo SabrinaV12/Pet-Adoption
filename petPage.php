6add9c5a1063f1bb26f315fb3508a6fab8b2f74a
Maria Ilade <mariailadem@gmail.com>
2025-06-05 22:37:57 +0300

    Small updates

--- petPage.php
+++ petPage.php
@@ -5,26 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hosha's Page</title>
-    <link rel="stylesheet" href="petPage.css" />
+    <link rel="stylesheet" href="design/petPage.css" />
+     <link rel="stylesheet" href="design/footer.css" />
 </head>
 
 <body class="container">
-    <header class="header">
-        <img src="../assets/logo.png" alt="Logo" width="122" height="50" />
-        <nav>
-            <a href="home.html">Home</a>
-            <a href="petSearch.html">Adopt</a>
-            <a href="careGuide.html">Care Guide</a>
-            <a href="contact.html">Contact</a>
-            <a href="notification.html">Notifications</a>
-            <a href="login.html">Login</a> | <a href="register.html">Register</a>
-        </nav>
-    </header>
+    
+    <?php include 'components/header.php'; ?>
 
-    <div class="page">
-        <img src="../assets/banner.png" alt="Banner" class="banner" />
-    </div>
+    <section class="page">
+        <img src="assets/banner.png" alt="Banner" class="banner" />
+</section>
 
+    <?php include 'components/footer.php'; ?>
 </body>
 
 </html>
