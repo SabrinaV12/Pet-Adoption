f350ead4dfa4f1c7cdb7b41748b61afb7bc0ad56
Maria Ilade <mariailadem@gmail.com>
2025-06-05 22:40:51 +0300

    Made the header that will be used for mostly every page (WIP)

---
+++ components/header.php
@@ -0,0 +1,11 @@
+<header class="header">
+        <img src="assets/logo.png" alt="Logo" width="122" height="50" />
+        <nav>
+            <a href="home.html">Home</a>
+            <a href="petSearch.html">Adopt</a>
+            <a href="careGuide.html">Care Guide</a>
+            <a href="contact.html">Contact</a>
+            <a href="notification.html">Notifications</a>
+            <a href="login.html">Login</a> | <a href="register.html">Register</a>
+        </nav>
+    </header>
