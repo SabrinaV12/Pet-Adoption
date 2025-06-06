75aa8c71d39ea743f88d398349dad353ea4f7f83
Maria Ilade <mariailadem@gmail.com>
2025-06-06 22:16:41 +0300

    made the header. to be prettified

--- components/header.php
+++ components/header.php
@@ -1,11 +1,30 @@
 <header class="header">
-        <img src="assets/logo.png" alt="Logo" width="122" height="50" />
-        <nav>
-            <a href="home.html">Home</a>
-            <a href="petSearch.html">Adopt</a>
-            <a href="careGuide.html">Care Guide</a>
-            <a href="contact.html">Contact</a>
-            <a href="notification.html">Notifications</a>
-            <a href="login.html">Login</a> | <a href="register.html">Register</a>
+        <nav class="nav">
+            <a href="home.html"><img src="assets/logo.png" alt="Home" width="122" height="50" /></a>
+
+             <section class="main-buttons">
+                <a href="petSearch.html">Adopt</a>
+                <a href="careGuide.html">Care Guide</a>
+                <a href="contact.html">Contact</a>
+              </section>
+
+              <section class="information-buttons">
+            <a href="notification.html"><img src="assets/notifications.png" alt="Notifications" /></a>
+            <div class="auth-links">
+                 <a href="login.html">Login</a> | <a href="register.html">Register</a>
+            </div>
+              </section>
+
         </nav>
-    </header>
+        <section class="mini-navigation">
+            <a href="home.html">Home > </a>
+            <a href="petSearch.html"> Adopt </a>
+
+            <form class="search-bar" action="search.php" method="GET">
+                <input type="text" name="query" placeholder="Search..." />
+                <button type="submit">
+                    <img src="assets/search-glass.png" alt="Search" />
+                </button>
+            </form>
+        </section>
+</header>
