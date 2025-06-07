0ef2cdfd6e4b018225ade4262d2caa4bb5fd3c69
Maria Ilade <mariailadem@gmail.com>
2025-06-07 21:52:38 +0300

    Updates

--- components/header.php
+++ components/header.php
@@ -1,24 +1,24 @@
 <header class="header">
         <nav class="nav">
-            <a href="home.html"><img src="assets/logo.png" alt="Home" width="122" height="50" /></a>
+            <a href="home.php"><img src="assets/logo.png" alt="Home" width="122" height="50" /></a>
 
              <section class="main-buttons">
-                <a href="petSearch.html">Adopt</a>
-                <a href="careGuide.html">Care Guide</a>
-                <a href="contact.html">Contact</a>
+                <a href="petSearch.php">Adopt</a>
+                <a href="careGuide.php">Care Guide</a>
+                <a href="contact.php">Contact</a>
               </section>
 
               <section class="information-buttons">
-            <a href="notification.html"><img src="assets/notifications.png" alt="Notifications" /></a>
+            <a href="notification.php"><img src="assets/notifications.png" alt="Notifications" /></a>
             <div class="auth-links">
-                 <a href="login.html">Login</a> | <a href="register.html">Register</a>
+                 <a href="login.php">Login</a> | <a href="register.php">Register</a>
             </div>
               </section>
 
         </nav>
         <section class="mini-navigation">
-            <a href="home.html">Home > </a>
-            <a href="petSearch.html"> Adopt </a>
+            <a href="home.php">Home > </a>
+            <a href="petSearch.php"> Adopt </a>
 
             <form class="search-bar" action="search.php" method="GET">
                 <input type="text" name="query" placeholder="Search..." />
