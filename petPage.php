8735b6ab0fcf7d74d9befa0b9dae3696f60172fa
Maria Ilade <mariailadem@gmail.com>
2025-06-19 02:47:37 +0300

    made some progress on the pet page. WIP.

--- petPage.php
+++ petPage.php
@@ -20,7 +20,37 @@ require_once 'database/check_auth.php';
     <?php include 'components/header.php'; ?>
 
     <section class="page">
-        <img src="assets/banner.png" alt="Banner" class="banner" />
+        <div class="banner_wrapper">
+            <img src="assets/banner.png" alt="Banner" class="banner" />
+            <img src="assets/profile_picture.png" alt="ProfilePicture" class="profile_picture" />
+        </div>
+
+        <section class="pet_details">
+
+            <div class="main_content">
+                <h1 class="big_text">Magie</h1>
+                <p class="small_text">Pet ID: 80638810</p>
+                <a href="adoptionStart.php" class="adopt_button">
+                    <span>Adopt</span>
+                    <img src="assets/arrow_circle_right.png" alt="Adopt" />
+                </a>
+            </div>
+
+            <div class="location_info">
+                <img src="assets/USA_flag.png" alt="USA Flag" class="location_info_flag" />
+
+                <div class="location_text_block">
+                    <div class="location_line">
+                        <span class="small_text">North America</span>
+                    </div>
+                    <div class="location_line">
+                        <img src="assets/location_ping.png" alt="Location pin" class="location_pin_icon" />
+                        <span class="small_text">California</span>
+                    </div>
+                </div>
+            </div>
+
+        </section>
     </section>
 
     <?php include 'components/footer.php'; ?>
