50ee1e9c72ebb32b3990ae23591460670ad585c0
SabrinaV12 <sabrinaburca5a@yahoo.com>
2025-06-24 02:15:46 +0300

    modificat footer

--- components/footer.php
+++ components/footer.php
@@ -5,29 +5,27 @@
 
         <section class="information-block"> 
         <p class="title-text"> How Can We Help? </p>
-        <nav class="nav-list"> <!-- TO DO: ADD relevant links -->
+        <nav class="nav-list">
             <a href="#">Adopt a pet</a>
             <a href="#">Rehome a pet</a>
             <a href="#">Adopt FAQ's</a>
             <a href="#">Rehome FAQ's</a>
-        </nav> <!-- TO DO: ADD relevant links -->
+        </nav>
         </section>
 
         <section class="information-block"> 
         <p class="title-text"> Contact Us </p>
-        <nav class="nav-list"> <!-- TO DO: ADD relevant links -->
+        <nav class="nav-list">
             <a href="#">Strada General Henri Mathias Berthelot 16, Iași</a>
             <a href="#">+40 712 345 678</a>
-            <a href="#">johndoe@gmail.com</a>
-        </nav> <!-- TO DO: ADD relevant links -->
+        </nav>
         </section>
 
         <section class="information-block"> 
         <p class="title-text"> Keep In Touch With Us </p>
-            <div class="nav-list"> <!-- TO DO: ADD relevant links -->
+            <div class="nav-list">
             <p>Join the FurryFriends magazine and be first to hear about news</p>
-            <a href="#">TO DO: PUT TEXTBOX</a>
-            </div> <!-- TO DO: ADD relevant links -->
+            </div>
         </section>
         
     </section>
